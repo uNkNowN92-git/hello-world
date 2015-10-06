107986ab98d1ce8c7ebbb557aaef6114bf86811d
uNkNowN92-git <uNkNowN92-git@users.noreply.github.com>
2015-10-06 18:04:28 +0800

    Update helloworld.php

--- helloworld.php
+++ helloworld.php
@@ -1,5 +1,16 @@
+<!DOCTYPE html>
+<head>
+  <meta http-equiv="cache-control" content="max-age=0" />
+  <meta http-equiv="cache-control" content="no-cache" />
+  <meta http-equiv="expires" content="0" />
+  <meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+  <meta http-equiv="pragma" content="no-cache" />
+</head>
+<body>
 <?php
   echo 'Hello, world!\n';
   echo date('F j, Y g:i:s A');
 ?>
 <h1>World Hello!</h1>
+</body>
+</html>
